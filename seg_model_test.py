a6ef20f9a343b748d8c584bb8a180a5a3ee11c93
LingRui Zhang <lingrui.ai@outlook.com>
2018-04-09 11:34:58 +0800

    增加NER提取代码

--- seg_model_test.py
+++ seg_model_test.py
@@ -10,51 +10,53 @@ from tensorflow.contrib import crf
 
 
 class BiLSTMTest(object):
-    def __init__(self, data=None, model_path='ckpt/seg',
-                 test_file='test/test.txt', test_result='test/seg_result.txt'):
+    def __init__(self, data=None, model_path='ckpt/ner0327',
+                 test_file='test/jyaq.csv', test_result='test/seg_result.txt'):
         self.data = data
         self.model_path = model_path
         self.test_file = test_file
         self.test_result = test_result
         self.sess = tf.Session()
         self.isload = self.loadModel(self.sess)
-
-    #  文字转ID
+ 
     def text2ids(self, text):
-        """把字片段text转为 ids."""
         words = list(text)
-        ids = list(self.data.word2id[words].fillna(self.data.word2id['<NEW>']))  # 找不到的词用特殊ID替换
-        # ids = list(self.data.word2id[words])
-        if len(ids) >= self.data.max_len:  # 长则弃掉
-            print(u'输出片段超过%d部分无法处理' % (self.data.max_len))
-            return ids[:self.data.max_len]
-        ids.extend([0] * (self.data.max_len - len(ids)))  # 短则补全
-        ids = np.asarray(ids).reshape([-1, self.data.max_len])
-        return ids
+        ids = list(self.data.word2id[words].fillna(self.data.word2id['<NEW>']))  
 
-    # 对于一个句子进行切分
+        if len(ids) >= self.data.max_len: 
+            print(u'输入句长超过%d，无法完成处理！' % (self.data.max_len))
+            ids = ids[:self.data.max_len]
+            ids = np.asarray(ids).reshape([-1, self.data.max_len])
+            return ids
+        else:
+            ids.extend([0] * (self.data.max_len - len(ids))) 
+            ids = np.asarray(ids).reshape([-1, self.data.max_len])
+            return ids
+
     def simple_cut(self, text, sess=None):
-        """对一个片段text（标点符号把句子划分为多个片段）进行预测。"""
         if text:
-            X_batch = self.text2ids(text)  # 这里每个 batch 是一个样本
-
+            X_batch = self.text2ids(text) 
+     
             fetches = [self.model.scores, self.model.length, self.model.transition_params]
             feed_dict = {self.model.X_inputs: X_batch, self.model.lr: 1.0, self.model.batch_size: 1,
                          self.model.keep_prob: 1.0}
-            test_score, test_length, transition_params = sess.run(fetches, feed_dict) # padding填充的部分直接丢弃
+            test_score, test_length, transition_params = sess.run(fetches, feed_dict) 
             tags, _ = crf.viterbi_decode(
                 test_score[0][:test_length[0]], transition_params)
             
 
             tags = list(self.data.id2tag[tags])
-            if tags[0] = TagSurfix.S.value and not tags[0].endswith(TagSurfix.B.value):
-                tags[0] = TagSurfix.S.value    #强制转换
             
+            # force change which the begin is not [s] or [b] , this error may show the RNN mistakes.
+            if tags[0] != TagSurfix.S.value and not tags[0].endswith(TagSurfix.B.value): 
+                tags[0] = TagSurfix.S.value
+ 
             words = []
-            for i in range(len(text)):  # 按照标签分割开
+            for i in range(len(tags)):
+           	 
                 if tags[i] == TagSurfix.S.value or tags[i].endswith(TagSurfix.B.value):
                     if tags[i].endswith('_' + TagSurfix.B.value):
-                        words.append([text[i], tags[i][:tags[i].find('_')]]) # 带上所属的标签
+                        words.append([text[i], tags[i][:tags[i].find('_')]]) 
                     else:
                         words.append(text[i])
                 else:
@@ -67,60 +69,127 @@ class BiLSTMTest(object):
         else:
             return []
 
-    # 切分内容原文
+    # 分句方式 1
     def cut_word(self, sentence):
-        """首先将一个sentence根据标点和英文符号/字符串划分成多个片段text，然后对每一个片段分词。"""
-        not_cuts = re.compile(u'[。？！.\?!]')
+        not_cuts = re.compile(u'[。.？！\?!]')
         result = []
         start = 0
 
         for seg_sign in not_cuts.finditer(sentence):
             result.extend(self.simple_cut(sentence[start:seg_sign.start()], self.sess))
-            result.append(sentence[seg_sign.start():seg_sign.end()])  # 追加相关分割符
+            result.append(sentence[seg_sign.start():seg_sign.end()]) 
             start = seg_sign.end()
         result.extend(self.simple_cut(sentence[start:], self.sess))
-
+        
         return result
+    
+    # 分句方式 2
+    def cut_sentence(self, line, max_len):
+        newLine = []
+        tags_result = []
+        # 小写转大写、去空格、去/n /t
+        line = line.replace(' ','').strip('"').replace('"','“').replace('!','！').replace('?','？').strip()
+        print(' ')
+        print('TEXT:',line)
+        # 去段尾句号
+        line = line.strip('.').strip('。') 
+        # 分句，还原句号
+        line = re.split(r'[。？！]', line)
+        line = [i + '。' for i in line]
+        
+            # 超过最大句长max_len进行逗号分句
+        def segSent(sentence):
+            sli = sentence.split('，')
+            lenSli = [ len(i) for i in sli]
+            sLen = 0
+            for i in lenSli:
+                sLen += i+1
+                if sLen > max_len:
+                    sLen -= i+1
+                    break
+            newLine.append(sentence[:sLen])
+            if len(sentence[sLen:]) > max_len:
+                segSent(sentence[sLen:])
+            else:
+                newLine.append(sentence[sLen:])
+            return newLine
 
-    # 读取测试文件并且写入测试文件
+        for sentence in line:
+            if len(sentence) > max_len:
+                segSent(sentence)
+            else:
+                newLine.append(sentence)
+        
+        for sentence in newLine: 
+            tags_result.extend(self.simple_cut(sentence, self.sess))
+    
+        return tags_result
+
+
     def testfile(self):
         isFile = os.path.isfile(self.test_file)
         if isFile:
-            with open(self.test_result, "w", encoding='utf-8') as out:  # 读写文件默认都是UTF-8编码的
+            with open(self.test_result, "w", encoding='utf-8') as out: 
                 with open(self.test_file, "r", encoding='utf-8') as fp:
                     for line in fp.readlines():
-                        print(line.strip())
-                        result = self.cut_word(line.strip())
-
+                        line = line.encode('utf-8').decode('utf-8-sig')
+                       # 分句方式 1 
+                       # line = line.strip('"')
+                       # line = line.replace('"','“').replace('"','”').replace(' ','')
+                       # result = self.cut_word(line.strip())
+                       #
+                       # 分句方式 2 
+                        result = self.cut_sentence(line, self.data.max_len)
+                        
                         rss = ''
-                        for each in result:
-                            if isinstance(each, list):
-                                rss = rss + each[0] + ' /' + each[1] + ' '
+                        DAT = ''
+                        LOC = ''
+                        PEO = []
+
+                        for each in result: 
+                            if isinstance(each, list):     
+                                # rss = rss +each[0] + '/' # no NER
+                                rss = rss + each[0] + ' /' + each[1] + ' ' # NER
+
+                                if each[1] == 'DAT':
+                                    DAT = DAT + each[0]
+                                elif each[1] == 'LOC':
+                                    LOC = LOC + each[0]
+                                elif each[1] == 'PEO':
+                                    PEO.append(each[0])
+
                             else:
-                                rss = rss + each + ' / '
+                                rss = rss + each + '/'
+                        
+                        # 人名去重
+                        PEO = list(set(PEO))
+                        PE0 = ''
+                        for i in PEO:
+                            PE0 = PE0 + i + ' '
 
-                        print(rss)
-                        out.write("%s\n" % (rss))
+                        print('DATE:',DAT)
+                        print('LOCTION:',LOC) 
+                        print('PEOPLE:',PE0)
+                        print(' ')
+                        #print(rss)
+                        #out.write("%s\n" % (rss))
 
-    # 加载还原模型
     def loadModel(self, sess=None):
         isload = False
-        # 再次构建模型, 然后恢复数据
         self.model = modelDef.BiLSTMModel(vocab_size=self.data.word2id.__len__(), class_num=self.data.tag2id.__len__())
         ckpt = tf.train.latest_checkpoint(self.model_path)
-        print(ckpt)
+        print(u'加载模型checkpoint完成，路径为：'+ckpt+'\n')
         saver = tf.train.Saver()
         if ckpt:
             saver.restore(sess, ckpt)
             isload = True
         return isload
 
-
 if __name__ == '__main__':
 
-    data = readData.DataHandler(save_path = 'data/SEG_training_data.pkl')
+    data = readData.DataHandler(save_path = 'data/data_ner_0327.pkl')
     data.loadData()
-    print('加载字典(分词)完成')
+    print(u'加载字典完成！\n')
     test = BiLSTMTest(data)
 
     if test.isload:
